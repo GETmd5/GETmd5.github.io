3f4e0fc16a48d61b73176c837c436d22a93667e9
GETmd5 <getmd5@outlook.com>
2013-06-29 10:40:14 +0200

    initial commit

---
+++ js/onReady.js
@@ -0,0 +1,1 @@
+function randomColour(){var letters='0123456789ABCD'.split('');var color='#';for(var i=0;i<6;i++){color+=letters[Math.round(Math.random()*13)]}return color}function doRequest(){var text=$("#string").val();$("#md5").val(md5(text))}$(document).ready(function(){$("#string").keyup(function(){setInterval(doRequest(),100)});$("body,html").css("background",randomColour())});
